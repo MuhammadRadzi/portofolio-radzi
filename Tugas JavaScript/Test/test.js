073d3ec4e807a7d8f3841fbdb9f215acc7337268
MuhammadRadzi <nippon.pein14@gmail.com>
2025-10-08 15:59:36 +0800

    Tugas JavaScript

---
+++ Tugas JavaScript/Test/test.js
@@ -0,0 +1,9 @@
+let nama = "Budi";    // bisa diubah
+const umur = 15;      // tidak bisa diubah
+var sekolah = "SMK";  // cara lama (tidak disarankan)
+
+if (umur >= 17) {
+	console.log("diatas umur");
+} else {
+	console.log("dibawah umur");
+}
