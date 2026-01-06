58936f1a3929983a5aa567949b9357ec45e384f5
MuhammadRadzi <nippon.pein14@gmail.com>
2026-01-06 10:23:15 +0800

    OOP and JavaScript

--- Tugas JavaScript/Test/test.js
+++
@@ -1,9 +0,0 @@
-let nama = "Budi";    // bisa diubah
-const umur = 15;      // tidak bisa diubah
-var sekolah = "SMK";  // cara lama (tidak disarankan)
-
-if (umur >= 17) {
-	console.log("diatas umur");
-} else {
-	console.log("dibawah umur");
-}
